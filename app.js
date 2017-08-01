e32bf3418c42443c63ff3775e5172d47f7b3095f
雅典 赤石 <aka.m6446@gmail.com>
2017-08-01 19:56:24 +0900

    add watson api

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var express = require('express');
+var fs = require('fs');
+if (fs.existsSync('./env.js')) {
+    Object.assign(process.env, require('./env.js'));
+}
 var cfenv = require('cfenv');
+var appEnv = cfenv.getAppEnv();
 var app = express();
+var watson = require('watson-developer-cloud');
+console.log(appEnv);
+var vr_credentials = appEnv.getServiceCreds('visual-recognition-1');
+console.log(vr_credentials);
+var visualRecognition = new watson.VisualRecognitionV3({
+  version_date: '2015-05-19',
+  api_key: vr_credentials.api_key
+});
 app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 app.listen(appEnv.port, '0.0.0.0', function() {
