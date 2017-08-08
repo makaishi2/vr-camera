c1eecd75a63522e4588ae7670669b75c743ec5a2
雅典 赤石 <aka.m6446@gmail.com>
2017-08-08 21:07:23 +0900

    remove gomi logic

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (fs.existsSync('./env.js')) {
 }
 
 var appEnv = cfenv.getAppEnv();
-var vr_credentials = appEnv.getServiceCreds('/visualrecognition-/');
+var vr_credentials = appEnv.getServiceCreds('vr-service-1');
 var visualRecognition = new watson.VisualRecognitionV3({
     version_date: '2015-05-19',
     api_key: vr_credentials.api_key
